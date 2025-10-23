97c2386c6383e8b307925407f5ed964b0a53f19c
Rick Alves Domingues <rickpoterdom@gmail.com>
2025-10-22 23:55:56 -0300

    projeto finalizado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "riqinho.com.github.imc_app"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "riqinho.com.github.imc_app"
